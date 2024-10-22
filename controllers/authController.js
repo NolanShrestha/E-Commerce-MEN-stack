6472b51a0b323dd168569b30b6672c8e56f94139
Nolan Shrestha <96360893+NolanShrestha@users.noreply.github.com>
2024-10-22 13:27:47 +0545

    Minor changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const JWT_SECRET = '3sIueX5FbB9B1G4vX9+OwI7zFt/P9FPW3sLd0R9MxHQ=';
 
 exports.register = async (req, res) => {
   const { username, email, password, phone, role, balance } = req.body;
-  console.log(username + email + password + phone + role + balance );
+  console.log(username + email + password + phone + role + balance);
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -33,201 +33,201 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const user = await User.findOne({ email});
-      console.log(email);
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found!' });
-      }
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-  
-      if (!isMatch) {
-        return res.status(400).json({ error: 'Invalid credentials!' });
-      }
-  
-      const token = jwt.sign({ id: user.id }, JWT_SECRET, {
-        expiresIn: '1h',
-      });
-      res.status(201).json({ message: 'Login Successful!' });
-    } catch (error) {
-      res.status(400).json({ error: 'Login failed!' });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    console.log(email);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found!' });
     }
-  };
 
-  exports.update = async (req, res) => {
-    const { email, currentPassword, newUsername, newPassword, newPhone } = req.body;
-  
-    console.log('Incoming request:', { email, currentPassword, newUsername, newPassword, newPhone });
-  
-    try {
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found!' });
-      }
-  
-      const isMatch = await bcrypt.compare(currentPassword, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ error: 'Invalid current password!' });
-      }
-  
-      if (newUsername) {
-        user.username = newUsername;
-        console.log('Username updated:', newUsername);
-      }
-  
-      if (newPassword) {
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        user.password = hashedPassword;
-        console.log('Password updated:', newPassword);
-      }
-  
-      if (newPhone) {
-        user.phone = newPhone;
-        console.log('Phone updated:', newPhone);
-      }
-  
-      await user.save();
-  
-      const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
-  
-      res.status(200).json({
-        message: 'User information updated successfully!',
-        token,
-        userId: user._id,
-        updatedUser: {
-          username: user.username,
-          email: user.email,
-          phone: user.phone,
-        }
-      });
-    } catch (error) {
-      console.error('Error updating user:', error);
-      res.status(500).json({ error: 'Failed to update user information!' });
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid credentials!' });
     }
-  };
 
-  exports.addProduct =  async (req, res) => {
-    const { email, name, description, price, stock } = req.body;
-    try {
-        const user = await User.findOne({ email });
-
-        if (!user || user.role!=="admin") {
-        return res.status(403).json({ message: 'Access denied!' });
-      }
-      const product = new Product({
-        name,
-        description,
-        price,
-        stock
-      });
-  
-      await product.save();
-  
-      res.status(201).json({ message: 'Product added successfully!', product });
-    } catch (error) {
-      console.error('Error adding product:', error);
-      res.status(500).json({ message: 'Failed to add product!', error: error.message });
-    }
-  };
-  
-  exports.payment = async (req, res) => {
-    const { email, productName, quantity } = req.body;
-    console.log(email +" "+ productName + " "+ quantity);
-  
-    try {
-      const user = await User.findOne({ email });
-      const product = await Product.findOne({ name: productName });
-  
-      if (!user) {
-        return res.status(404).json({ error: 'User not found!' });
-      }
-  
-      if (!product) {
-        return res.status(404).json({ error: 'Invalid Product!' });
-      }
-  
-      if (product.stock < quantity) {
-        return res.status(400).json({ error: 'Insufficient stock!' });
-      }
-  
-      const totalAmount = product.price * quantity;
-  
-      if (user.balance >= totalAmount) {
-        user.balance -= totalAmount;
-        product.stock -= quantity;
-  
-        const payment = new Payment({
-          userId: user._id,
-          productId: product._id,
-          amount: totalAmount,
-        });
-  
-        await payment.save(); 
-        await user.save(); 
-        await product.save(); 
-  
-        return res.status(200).json({
-          message: 'Payment successful!',
-          updatedBalance: user.balance,
-          paymentDetails: {
-            amount: payment.amount,
-            quantity,
-          },
-        });
-      } else {
-        return res.status(400).json({ error: 'Insufficient balance!' });
-      }
-    } catch (error) {
-      console.error('Payment error:', error);
-      res.status(500).json({ error: 'Payment failed!' });
-    }
-  };
-
-exports.review = async (req, res) => {
-    const { productName, email, rating, reviewText } = req.body;
-
-    try {
-      const user = await User.findOne({ email });
-      const product = await Product.findOne({ name: productName });
-
-        if (!product) {
-            return res.status(404).json({ error: 'Product not found!' });
-        }
-        if (!user) {
-            return res.status(404).json({ error: 'User not found!' });
-        }
-
-        const review = new Review({
-            productId: product._id,
-            userId: user._id,
-            rating,
-            reviewText
-        });
-
-        await review.save();
-
-        res.status(201).json({
-            message: 'Review submitted successfully!',
-            review: {
-                productId: review.productId,
-                userId: review.userId,
-                rating: review.rating,
-                reviewText: review.reviewText
-            }
-        });
-    } catch (error) {
-        console.error('Error submitting review:', error);
-        res.status(500).json({ error: 'Failed to submit review!' });
-    }
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    res.status(201).json({ message: 'Login Successful!' });
+  } catch (error) {
+    res.status(400).json({ error: 'Login failed!' });
+  }
 };
 
-  
-  
+exports.update = async (req, res) => {
+  const { email, currentPassword, newUsername, newPassword, newPhone } = req.body;
+
+  console.log('Incoming request:', { email, currentPassword, newUsername, newPassword, newPhone });
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found!' });
+    }
+
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid current password!' });
+    }
+
+    if (newUsername) {
+      user.username = newUsername;
+      console.log('Username updated:', newUsername);
+    }
+
+    if (newPassword) {
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.password = hashedPassword;
+      console.log('Password updated:', newPassword);
+    }
+
+    if (newPhone) {
+      user.phone = newPhone;
+      console.log('Phone updated:', newPhone);
+    }
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({
+      message: 'User information updated successfully!',
+      token,
+      userId: user._id,
+      updatedUser: {
+        username: user.username,
+        email: user.email,
+        phone: user.phone,
+      }
+    });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ error: 'Failed to update user information!' });
+  }
+};
+
+exports.addProduct = async (req, res) => {
+  const { email, name, description, price, stock } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user || user.role !== "admin") {
+      return res.status(403).json({ message: 'Access denied!' });
+    }
+    const product = new Product({
+      name,
+      description,
+      price,
+      stock
+    });
+
+    await product.save();
+
+    res.status(201).json({ message: 'Product added successfully!', product });
+  } catch (error) {
+    console.error('Error adding product:', error);
+    res.status(500).json({ message: 'Failed to add product!', error: error.message });
+  }
+};
+
+exports.payment = async (req, res) => {
+  const { email, productName, quantity } = req.body;
+  console.log(email + " " + productName + " " + quantity);
+
+  try {
+    const user = await User.findOne({ email });
+    const product = await Product.findOne({ name: productName });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found!' });
+    }
+
+    if (!product) {
+      return res.status(404).json({ error: 'Invalid Product!' });
+    }
+
+    if (product.stock < quantity) {
+      return res.status(400).json({ error: 'Insufficient stock!' });
+    }
+
+    const totalAmount = product.price * quantity;
+
+    if (user.balance >= totalAmount) {
+      user.balance -= totalAmount;
+      product.stock -= quantity;
+
+      const payment = new Payment({
+        userId: user._id,
+        productId: product._id,
+        amount: totalAmount,
+      });
+
+      await payment.save();
+      await user.save();
+      await product.save();
+
+      return res.status(200).json({
+        message: 'Payment successful!',
+        updatedBalance: user.balance,
+        paymentDetails: {
+          amount: payment.amount,
+          quantity,
+        },
+      });
+    } else {
+      return res.status(400).json({ error: 'Insufficient balance!' });
+    }
+  } catch (error) {
+    console.error('Payment error:', error);
+    res.status(500).json({ error: 'Payment failed!' });
+  }
+};
+
+exports.review = async (req, res) => {
+  const { productName, email, rating, reviewText } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    const product = await Product.findOne({ name: productName });
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found!' });
+    }
+    if (!user) {
+      return res.status(404).json({ error: 'User not found!' });
+    }
+
+    const review = new Review({
+      productId: product._id,
+      userId: user._id,
+      rating,
+      reviewText
+    });
+
+    await review.save();
+
+    res.status(201).json({
+      message: 'Review submitted successfully!',
+      review: {
+        productId: review.productId,
+        userId: review.userId,
+        rating: review.rating,
+        reviewText: review.reviewText
+      }
+    });
+  } catch (error) {
+    console.error('Error submitting review:', error);
+    res.status(500).json({ error: 'Failed to submit review!' });
+  }
+};
+
+
+
 
 
 
